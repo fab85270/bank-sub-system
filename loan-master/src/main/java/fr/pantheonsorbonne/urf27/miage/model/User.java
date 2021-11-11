b4cc140bdd5ae8a736f643e27cadc8bd405f32d1
fab85270 <coutanceauf85@gmail.com>
2021-11-11 13:33:05 +0100

    e

--- loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/User.java
+++ loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/User.java
@@ -11,5 +11,58 @@ public class User {
     private String firstName;
     private String lastName;
     private String mail;
-    private
+
+    /*Constructeur de la classe User*/
+
+    public User(){}
+
+    public User(String firstName,String lastName, String mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+
+    /*Getters/Setters de la classe*/
 }
