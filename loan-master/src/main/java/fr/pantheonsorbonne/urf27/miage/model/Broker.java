992aed95a3f496961320d6a69eb17587a6d5928b
fab85270 <coutanceauf85@gmail.com>
2021-11-11 13:40:12 +0100

    e

--- loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/Broker.java
+++ loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/Broker.java
@@ -4,7 +4,25 @@ import javax.persistence.*;
 import java.util.List;
 import javax.ws.rs.client.Client;
 
-@Entity
-public class Broker {
 
+@Entity
+public class Broker extends User{
+    /*Attributs privés propres à la classe User*/
+
+    public enum Role {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idClient;
+    private Role role; //Ou sinon ici mettre un type énuméré en dehors de la classe ? A voir!
+
+    /*Constructeur de la classe Broker*/
+    public Broker(){}
+
+    public Broker(String firstName,String lastName,String mail,Role role){
+        super(firstName,lastName,mail);
+        this.role = role; //Ici ajouter d'autres attributs que rôle pour le broker ??
+    }
 }
