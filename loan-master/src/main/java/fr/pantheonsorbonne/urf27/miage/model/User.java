02bdca6ccaa20caa93c0b59cc983654777b7a142
fab85270 <coutanceauf85@gmail.com>
2021-11-11 13:30:34 +0100

    e

--- loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/User.java
+++ loan-master/src/main/java/fr/pantheonsorbonne/urf27/miage/model/User.java
@@ -1,4 +1,15 @@
 package fr.pantheonsorbonne.urf27.miage.model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    /* Variables privées propres à une Bank*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private
 }
