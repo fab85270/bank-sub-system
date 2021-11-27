161859602988d3a9f079604a32b5238e6e84b770
fab85270 <coutanceauf85@gmail.com>
2021-11-27 01:06:11 +0100

    modifScript.js

--- loan-broker/src/main/resources/META-INF/resources/script.js
+++ loan-broker/src/main/resources/META-INF/resources/script.js
@@ -4,14 +4,23 @@ function controle(){
     let dateFin = document.getElementById("dateFin").value;
     let sommeVoulu = document.getElementById("sommeVoulu").value;
     let workStatut = document.getElementById("workStatut").value;
+
     let date = new Date();
 
+
+    //console.log(date);
+    console.log("coucu");
     //On converti une valeur javascript en chaine JSON
-    let isDateDepart = (dateDepart>=date.getFullYear());
+    let isDateDepart = (dateDepart>=date);
+
+    console.log(isDateDepart);
     //let isDateFin = (dateFin>dateDepart && dateFin<9999);
     let isSommeVoulu = sommeVoulu>0;
+    console.log("coucu");
 
-    if(isDateDepart && isSommeVoulu){
+    //if(isDateDepart && isSommeVoulu){
+        console.log("coucu"); //On rentre jamais ici
+
         let _data = {
             description: description,
             dateDepart: dateDepart,
@@ -26,9 +35,9 @@ function controle(){
         xmlhttp.open("POST", "/broker/createProjectClient");
         xmlhttp.setRequestHeader("Content-Type", "application/json");
         xmlhttp.send(JSON.stringify(_data));
-    }else{
+   // }else{
         //if(!isDateDepart){window.alert("Error : la date de départ doit être supérieur à "+date.getFullYear()};
         if(!isSommeVoulu){window.alert("Error : la somme doit être supérieur à 0")}
-    }
+   // }
 }
 
