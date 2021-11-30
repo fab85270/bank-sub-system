752c7f3efa73f319fcb1d516ef63ba37abbb1499
fab85270 <coutanceauf85@gmail.com>
2021-11-30 20:40:03 +0100

    Modif30nov

--- loan-commons/src/main/java/loan/commons/dto/SelectProjetBankDTO.java
+++ loan-commons/src/main/java/loan/commons/dto/SelectProjetBankDTO.java
@@ -1,2 +1,33 @@
-package loan.commons.dto;public class SelectProjetBankDTO {
+package loan.commons.dto;
+
+/* Il s'agira du DTO contenant le numéro(identifiant) du project Client qui sera envoyé à la Bank*/
+
+public class SelectProjetBankDTO {
+    int idProject;
+    int idBank;
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    @Override
+    public String toString() {
+        return "SelectProjetBankDTO{" +
+                "idProject=" + idProject +
+                ", idBank=" + idBank +
+                '}';
+    }
+
+    public int getIdBank() {
+        return idBank;
+    }
+
+    public void setIdBank(int idBank) {
+        this.idBank = idBank;
+    }
 }
